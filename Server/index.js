6d4ae5e8895b5bc1df36dfef2429fdbbf596bc21
Anuj Mehta <anujmehta128@gmail.com>
2025-10-09 19:01:35 +0530

    Fix: updated index.js with new functionality

--- Server/index.js
+++ Server/index.js
@@ -13,7 +13,7 @@ const {router:bookingRouter}=require('./routes/booking');
 
 const { connectClodinary } = require('./configs/cloudinary');
 
-const PORT=3000;
+const PORT=process.env.PORT|| 3000;
 
 const app=express();
 
