1fe4fd1f3dd774aeb7ccdd476e75216c1c2d70b2
Anuj Mehta <anujmehta128@gmail.com>
2025-10-09 21:15:37 +0530

    mongo db function restored

--- Server/configs/connection.js
+++ Server/configs/connection.js
@@ -1,9 +1,9 @@
 const mongoose=require('mongoose');
 
 
-async function handleMongodbConnectionRequest()
+function handleMongodbConnectionRequest()
 {
-    await mongoose.connect(`${process.env.MONGODB_URL}/hotel-booking-application`)
+    mongoose.connect(`${process.env.MONGODB_URL}/hotel-booking-application`)
     .then(()=>{console.log("mongodb connected")})
     .catch((err)=>{console.log("mongo error :",err)});
 
