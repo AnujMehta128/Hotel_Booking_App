4b18ba0744558a7c70742a49254f043e0188311d
Anuj Mehta <anujmehta128@gmail.com>
2025-10-10 11:29:40 +0530

    navbar anchor tag updated to link tag

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -104,10 +104,10 @@ const {userData,backendURL,setUserData,setIsLoggedIn ,isOwner,setShowHotelRegist
                 {/* Desktop Nav */}
                 <div className="hidden md:flex items-center gap-4 lg:gap-8">
                     {navLinks.map((link, i) => (
-                        <a key={i} href={link.path} className={`group flex flex-col gap-0.5 ${isScrolled ? "text-gray-700" : "text-white"}`}>
+                        <Link key={i} to={link.path} className={`group flex flex-col gap-0.5 ${isScrolled ? "text-gray-700" : "text-white"}`}>
                             {link.name}
                             <div className={`${isScrolled ? "bg-gray-700" : "bg-white"} h-0.5 w-0 group-hover:w-full transition-all duration-300`} />
-                        </a>
+                        </Link>
                     ))}
 
                     {/* is user data is availabel only then dashboard button appears if user data is there but user is not hthe owner list your hotel button appears 
@@ -163,9 +163,9 @@ const {userData,backendURL,setUserData,setIsLoggedIn ,isOwner,setShowHotelRegist
                     </button>
 
                     {navLinks.map((link, i) => (
-                        <a key={i} href={link.path} onClick={() => setIsMenuOpen(false)}>
+                        <Link key={i} to={link.path} onClick={() => setIsMenuOpen(false)}>
                             {link.name}
-                        </a>
+                        </Link>
                     ))}
 
                     {/* display it only when the user is logged In */}
